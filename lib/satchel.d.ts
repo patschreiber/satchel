e21099e895a8dff1a3fc185a696afe74c0ad11a6
Patrick Schreiber <2431707+patschreiber@users.noreply.github.com>
2023-06-24 18:15:45 -0500

    Added dep uuid

--- lib/satchel.d.ts
+++ lib/satchel.d.ts
@@ -1,3 +1,12 @@
 declare namespace Satchel {
+  import { v4 as uuidv4 } from 'uuid';
+
   type Coordinate = [number, number];
+
+  // Wrap the dep so it's swappable l8r.
+  type uuid = uuidv4;
+
+  type Container = {
+    id: uuid
+  }
 }
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+// Not to be confused with the import x = require("name") syntax used to load
+// modules, this syntax simply creates an alias for the specified symbol. You
+// can use these sorts of imports (commonly referred to as aliases) for any kind
+// of identifier, including objects created from module imports.
+import S = Satchel;
